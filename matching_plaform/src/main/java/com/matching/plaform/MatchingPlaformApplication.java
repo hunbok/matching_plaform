a7c2921b906642d3bf5a45eee4bfc8a1fb0a9999
6A <6A@DESKTOP-1R7EAI6>
2024-12-04 16:09:57 +0900

    프로젝트 최초 생성

---
+++ matching_plaform/src/main/java/com/matching/plaform/MatchingPlaformApplication.java
@@ -0,0 +1,13 @@
+package com.matching.plaform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingPlaformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingPlaformApplication.class, args);
+	}
+
+}
