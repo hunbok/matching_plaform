5a105e83d3279a9dfa72d133d0db4d649e82a77c
hunbok <rmsidrmfo1537@gmail.com>
2024-12-04 18:17:32 +0900

    2024-12-04 학원 최종

--- matching_plaform/src/main/java/com/matching/plaform/domain/Category.java
+++ matching_plaform/src/main/java/com/matching/plaform/domain/Category.java
@@ -1,5 +1,14 @@
 package com.matching.plaform.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 	
 	private int category_code;
--- matching_plaform/src/main/java/com/matching/plaform/domain/Reply.java
+++ matching_plaform/src/main/java/com/matching/plaform/domain/Reply.java
@@ -2,6 +2,15 @@ package com.matching.plaform.domain;
 
 import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reply {
 	
 	private int reply_no;
