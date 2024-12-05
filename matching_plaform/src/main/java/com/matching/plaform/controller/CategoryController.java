e60e81b0e072cb80a2423bbf142921e9a9d25963
gjsqh <gjsqh@DESKTOP-CLKMA44>
2024-12-06 01:26:23 +0900

    2024-12-06 home - 최종

--- matching_plaform/src/main/java/com/matching/plaform/controller/CategoryController.java
+++ matching_plaform/src/main/java/com/matching/plaform/controller/CategoryController.java
@@ -1,11 +1,14 @@
 package com.matching.plaform.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.matching.plaform.domain.Category;
 import com.matching.plaform.service.BoardService;
 import com.matching.plaform.service.CategoryService;
 
@@ -15,6 +18,11 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 	
-	
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        List<Category> categories = categoryService.categoryList();
+        model.addAttribute("categories", categories); 
+        return "homePage"; 
+    }
 
 }
