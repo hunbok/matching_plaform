910999f01243cd74c777f100ce5e4cc1000bd9c8
hunbok <rmsidrmfo1537@gmail.com>
2024-12-05 09:32:07 +0900

    SQL - 추가

---
+++ matching_plaform/src/main/resources/SQL/board.sql
@@ -0,0 +1,81 @@
+
+SHOW DATABASES;
+
+SHOW tables;
+
+USE category;
+USE member;
+USE board;
+USE reply;
+
+-- 카테고리
+DROP TABLE IF EXISTS category;
+CREATE TABLE IF NOT EXISTS category(
+category_code INTEGER AUTO_INCREMENT PRIMARY KEY,
+category_name VARCHAR(10) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO category(category_name) VALUES ('공지사항');
+
+-- 회원
+DROP TABLE IF EXISTS member;
+CREATE TABLE IF NOT EXISTS member(
+id VARCHAR(20) PRIMARY KEY,
+name VARCHAR(10) NOT NULL,
+pass VARCHAR(100) NOT NULL,
+email VARCHAR(30) NOT NULL,
+mobile VARCHAR(13) NOT NULL,
+zipcode  VARCHAR(5) NOT NULL,
+address1  VARCHAR(80) NOT NULL,
+address2  VARCHAR(60) NOT NULL,
+email_get VARCHAR(1) NOT NULL,
+reg_date  TIMESTAMP NOT NULL,
+nickname VARCHAR(20) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO member(id, name, pass, email, mobile, zipcode, address1, address2, email_get, reg_date, nickname)
+VALUES('bokbok', '이헌복', '1234', 'bokbok@naver.com', '010-1111-2222', '08787', '서울시 관악구 남부순환로 1820 (봉천동, 에그엘로우)', '6층', '0', '2022-12-22 11:44:58', '관리자');
+INSERT INTO member(id, name, pass, email, mobile, zipcode, address1, address2, email_get, reg_date, nickname)
+VALUES('midas', '원 진', '1234', 'midas@naver.com', '010-2222-3333', '08802', '서울시 관악구 남부순환로 1820 (봉천동, 에그엘로우)', '14층', '1', '2022-12-23 11:44:58', '미다스');
+
+
+
+-- 게시판
+DROP TABLE IF EXISTS board;
+CREATE TABLE IF NOT EXISTS board(
+board_no INTEGER AUTO_INCREMENT PRIMARY KEY,
+board_title VARCHAR(50) NOT NULL,
+board_content VARCHAR(10000) NOT NULL,
+board_reg_date TIMESTAMP NOT NULL,
+board_view INTEGER NOT NULL,
+category_code INTEGER NOT NULL,
+member_id VARCHAR(20) NOT NULL,
+board_file1 VARCHAR(100),
+board_like INTEGER DEFAULT 0,
+board_dislike INTEGER DEFAULT 0,
+CONSTRAINT member_id_fk FOREIGN KEY (member_id) REFERENCES member(id),
+CONSTRAINT category_code_fk FOREIGN KEY (category_code) REFERENCES category(category_code)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO board(board_title, board_content, board_reg_date, board_view, category_code, member_id, board_file1)
+VALUES('공지 사항 입니다.', '안녕하세요\r\n이번에 어쩌구 저쩌구 해서리...\r\n\r\n\r\r\n\n이렇게 운영계획과 약관을 바꾸게 되었습니다.\r\n\r\n회원님들의 양해를 구하며 사이트 이용해 참고 하시기 바랍니다.\r\n\r\n','2022-12-22 11:44:58', 102, 1, 'bokbok', null);
+
+-- 댓글
+DROP TABLE IF EXISTS reply;
+CREATE TABLE IF NOT EXISTS reply(
+   reply_no INTEGER AUTO_INCREMENT PRIMARY KEY,
+   board_no INTEGER NOT NULL,
+   reply_content VARCHAR(500) NOT NULL,
+   member_id VARCHAR(20) NOT NULL,
+   reply_reg_date TIMESTAMP NOT NULL,
+   reply_like INTEGER DEFAULT 0,
+   reply_dislike INTEGER DEFAULT 0,
+   CONSTRAINT board_no_reply_fk FOREIGN KEY(board_no) REFERENCES board(board_no),
+   CONSTRAINT reply_member_id_fk FOREIGN KEY(member_id) REFERENCES member(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO reply(board_no, reply_content, member_id, reply_reg_date) 
+VALUES(1, '고생하십니다.', 'midas', '2022-12-23 11:44:58');
+
+SELECT * FROM category ORDER BY category_code DESC;
+SELECT * FROM board ORDER BY board_no DESC;
